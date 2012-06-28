3d453aac33a57e9b0c5b5afaa3361711329b3508
Matt Hinze <mhinze@gmail.com>
2012-06-28 17:08:15 -0500

    code cleanup

--- Client/RedisClient.cs
+++ Client/RedisClient.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using Client.Replies;
 
 namespace Client
@@ -101,33 +99,6 @@ namespace Client
         public BulkReplyInfo Info()
         {
             return _connection.Send<BulkReply>("INFO");
-        }
-    }
-
-    public class BulkReplyInfo
-    {
-        readonly BulkReply _reply;
-        Dictionary<string, string> dictionary;
-
-        public BulkReplyInfo(BulkReply reply)
-        {
-            _reply = reply;
-        }
-
-        public string this[string key]
-        {
-            get
-            {
-                dictionary = dictionary ?? CreateDictionary();
-                return dictionary[key];
-            }
-        }
-
-        Dictionary<string, string> CreateDictionary()
-        {
-            var s = _reply.ToString();
-            var strings = s.Split('\r', '\n').Where(x => !string.IsNullOrWhiteSpace(x));
-            return strings.ToDictionary(x => x.Split(':')[0], x => x.Split(':')[1]);
         }
     }
 }
